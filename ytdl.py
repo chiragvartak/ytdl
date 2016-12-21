f52714d3381de258eb265018ac6c9dd37145cad2
Chirag Vartak <vartakchirag8@gmail.com>
2016-12-21 19:07:08 +0530

    Clean code

--- ytdl.py
+++ ytdl.py
@@ -2,25 +2,6 @@ import sys
 import json
 import string
 
-config_file = 'config.json'
-songs_file = ''
-HTTP_PROXY = ''
-API_KEY = ''
-
-if len(sys.argv) != 2:
-    print('Usage: ytdl.py <songs.txt>')
-
-songs_file = sys.argv[1]
-
-config_data = {}
-with open(config_file, 'r') as f:
-    config_data = json.load(f)
-if 'HTTP_PROXY' in config_data:
-    HTTP_PROXY = config_data['HTTP_PROXY']
-API_KEY = config_data['API_KEY']
-
-# TODO: check_network()
-
 def get_searchable_string(s):
     offensive_chars = string.punctuation
     offensive_words = ['song']
@@ -32,6 +13,26 @@ def get_searchable_string(s):
     return s
 
 if __name__ == '__main__':
+    config_file = 'config.json'
+    songs_file = ''
+    HTTP_PROXY = ''
+    API_KEY = ''
+
+    if len(sys.argv) != 2:
+        print('Usage: ytdl.py <songs.txt>')
+
+    songs_file = sys.argv[1]
+
+    config_data = {}
+    with open(config_file, 'r') as f:
+        config_data = json.load(f)
+    if 'HTTP_PROXY' in config_data:
+        HTTP_PROXY = config_data['HTTP_PROXY']
+    API_KEY = config_data['API_KEY']
+
+    # TODO
+    # check_network()
+
     with open(songs_file, 'r') as f:
         songs = f.read().split('\n')
     
